d41c37e9f35a719e2b248758e21f31865b35e155
HarryFray <harry.fray7@gmail.com>
2018-07-10 13:39:07 -0500

    Watch for changes on server / api routes created

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false,
+}));
 
 
 const eventsRouter = express.Router();
 app.use('/events', eventsRouter);
 
 
-eventsRouter.get('/', (req, res) => res.send('test'))
+// submit events
+eventsRouter.post('/', (req, res) => res.json({ events: true }))
+
+// clear data
+eventsRouter.post('/clear', (req, res) => res.json({ events: true }))
+
+// list events
+eventsRouter.get('/', (req, res) => res.json({ events: true }))
+
+// event summary
+eventsRouter.get('/', (req, res) => res.json({ events: true }))
+
 
 
 app.listen(8080, () => {
